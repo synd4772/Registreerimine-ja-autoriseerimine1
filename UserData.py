b1aead635749408184159c0b34924db0ecd1ea81
synd4772 <82279357+synd4772@users.noreply.github.com>
2024-03-07 18:15:52 +0200

    user data

---
+++ UserData.py
@@ -0,0 +1,13 @@
+user_data = [{
+    "user_id": 1,
+    "user_name": "Mark Piller",
+    "user_password": "SA4K1NM#MFDA",
+    "secret_word": "banana",
+    "email": "mark.piller@gmail.com"
+}, {
+    "user_id": 2,
+    "user_name": "John Doe",
+    "user_password": "das34SsDczxma",
+    "secret_word": "õun",
+    "email": "john.doe@gmail.com"
+}]
